d5aea52affa1b801d034694b2bbd489c599f39e2
arafatul-islam <marafatbs@gmail.com>
2024-10-09 12:50:22 +0600

    add: 1.servers/5.fileServer - assignment done with nodejs

--- assignments/server.js
+++ assignments/server.js
@@ -1,8 +1,24 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, resp) => {
-  resp.write("404 not found");
-  resp.end();
+  if (req.url == "/") {
+    const filePath = path.join(__dirname, "index.html");
+
+    fs.readFile(filePath, (error, content) => {
+      if (error) {
+        resp.writeHead(500, { "content-type": "text/plain" });
+        resp.end();
+      }
+
+      resp.writeHead(200, { "content-type": "text/plain" });
+      resp.end(content);
+    });
+  } else {
+    resp.writeHead(404, { "content-type": "text/plain" });
+    resp.end();
+  }
 });
 
-server.listen(8080);
+server.listen(8080, () => console.log(`server is listening`));
